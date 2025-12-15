fcf25dd920e2b2f43794ac9a88c5ef7ceecb69e2
submittohim420n@hotmail.com <submit4201>
2025-12-14 20:13:57 -0800

    feat: add engine package initialization and exports

---
+++ backend/engine/__init__.py
@@ -0,0 +1,8 @@
+"""
+__init__ file for engine module.
+"""
+
+from engine.game_engine import GameEngine
+from engine.autonomous_simulation import AutonomousSimulation
+
+__all__ = ["GameEngine", "AutonomousSimulation"]
